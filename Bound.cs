5d95d912f94219804c5fb22ca8a0f90a8f1ae8ab
shuai <zhaishuaigan@qq.com>
2021-01-01 20:26:37 +0800

    init

---
+++ Bound.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Espruino
+{
+    public class Bound
+    {
+        public Form app;
+        public Bound(Form app)
+        {
+            this.app = app;
+
+        }
+
+        public string SetTitle(string title)
+        {
+            this.app.Text = title;
+            return title;
+        }
+
+        public Form GetForm()
+        {
+            return this.app;
+        }
+
+        public string FunctionForHtml(string msg)
+        {
+            // 将Html传递过来的数据设置到组件文本框中
+            MessageBox.Show("收到Html的数据：" + msg);
+            // 返回给Html的数据
+            return "我已收到:" + msg;
+        }
+
+        public string[] GetPorts()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+        public string GetRootPath()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+
+        public string[] GetEspruinoBinList()
+        {
+            string binPath = GetRootPath() + "/bin/espruino/";
+            FileInfo[] files = new DirectoryInfo(binPath).GetFiles();
+            string[] result = new String[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                result[i] = files[i].Name;
+            }
+            return result;
+        }
+
+        public void DownloadEspruinoToEsp01s(string port)
+        {
+            var baud = "115200";
+            var bin = "./espruino/espruino_2v08_esp8266_combined_512.bin";
+            var flashSize = "512KB";
+            var argument = "--port " + port + " --baud " + baud + " write_flash --flash_size " + flashSize + " 0x0000 " + bin;
+            DownloadEspruino(argument);
+        }
+
+        public void DownloadEspruinoToEsp8266(string port)
+        {
+            var baud = "115200";
+            var bin = "./espruino/espruino_2v08_esp8266_4mb_combined_4096.bin";
+            var flashSize = "4MB";
+            var argument = "--port " + port + " --baud " + baud + " write_flash --flash_size " + flashSize + " 0x0000 " + bin;
+            DownloadEspruino(argument);
+        }
+        public void DownloadEspruino(string argument)
+        {
+
+            LaunchBat(Environment.CurrentDirectory + "/bin/espruino.bat", argument);
+        }
+
+        public void LaunchBat(string batName, string argument)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                FileName = batName,
+                Arguments = argument
+            };
+            // startInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            Process.Start(startInfo);
+        }
+
+        public void SendCode()
+        {
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,83 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Espruino
+{
+    public partial class Main : Form
+    {
+        public ChromiumWebBrowser browser;
+        public Main()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;//设置该属性 为false
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            browser = new ChromiumWebBrowser((new Bound(this)).GetRootPath() + "/view/index.html")
+            {
+                Dock = DockStyle.Fill
+            };
+            this.Controls.Add(browser);
+            browser.JavascriptObjectRepository.Register("bound", new Bound(this), true, BindingOptions.DefaultBinder);
+            browser.KeyboardHandler = new CEFKeyBoardHander();
+        }
+
+    }
+
+    public class CEFKeyBoardHander : IKeyboardHandler
+    {
+        public bool OnKeyEvent(IWebBrowser browserControl, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey)
+        {
+            if (type == KeyType.KeyUp && Enum.IsDefined(typeof(Keys), windowsKeyCode))
+            {
+                var key = (Keys)windowsKeyCode;
+                switch (key)
+                {
+                    case Keys.F12:
+                        browser.ShowDevTools();
+                        break;
+
+                    case Keys.F5:
+
+                        if (modifiers == CefEventFlags.ControlDown)
+                        {
+                            //MessageBox.Show("ctrl+f5");
+                            browser.Reload(true); //强制忽略缓存
+
+                        }
+                        else
+                        {
+                            //MessageBox.Show("f5");
+                            browser.Reload();
+                        }
+                        break;
+
+
+                }
+            }
+            return false;
+        }
+
+        public bool OnPreKeyEvent(IWebBrowser browserControl, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey, ref bool isKeyboardShortcut)
+        {
+            return false;
+        }
+    }
+
+
+
+    
+}
